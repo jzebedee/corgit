cfa6237aa590407af15e57c0e55ca42a472a2706
J. Zebedee <zebedee@code.gripe>
2019-02-14 11:17:59 -0600

    Add test ParseSimpleLongerStatus

--- test/corgit.tests/Git/StatusTests.cs
+++ test/corgit.tests/Git/StatusTests.cs
@@ -27,13 +27,24 @@ namespace corgit.tests
 
             var expected = new List<GitFileStatus>()
             {
-                new GitFileStatus()
-                {
-                    X = '?',
-                    Y = '?',
-                    Rename = null,
-                    Path = "file.txt"
-                }
+                ('?', '?', null, "file.txt")
+            };
+            var results = Git.ParseStatus(status);
+            Assert.Equal(expected, results);
+        }
+
+        [Fact]
+        public void ParseSimpleLongerStatus()
+        {
+            const string status = "?? file.txt\0"
+                                  + "?? file2.txt\0"
+                                  + "?? file3.txt\0";
+
+            var expected = new List<GitFileStatus>()
+            {
+                ('?', '?', null, "file.txt"),
+                ('?', '?', null, "file2.txt"),
+                ('?', '?', null, "file3.txt"),
             };
             var results = Git.ParseStatus(status);
             Assert.Equal(expected, results);
