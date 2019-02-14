6a8e4141ec9118e00eefa25818ef383285332596
J. Zebedee <zebedee@code.gripe>
2019-02-14 10:02:36 -0600

    Disable optional locks for Status

--- src/corgit/Corgit.cs
+++ src/corgit/Corgit.cs
@@ -111,8 +111,9 @@ namespace corgit
             _workingDirectory = workingDirectory;
         }
 
+        private static readonly Dictionary<string, string> StatusOptions = new Dictionary<string, string> { { "GIT_OPTIONAL_LOCKS", "0" } };
         public async Task<ExecutionResult> StatusAsync()
-            => await RunGitAsync(GitArguments.Status());
+            => await RunGitAsync(GitArguments.Status(), env: StatusOptions);
 
         public async Task<ExecutionResult> InitAsync()
             => await RunGitAsync(GitArguments.Init());
