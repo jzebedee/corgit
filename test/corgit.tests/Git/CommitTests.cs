0a811f6c3a269c21cf954da795e0c61985a03cd2
J. Zebedee <zebedee@code.gripe>
2019-02-14 04:48:00 -0600

    Add initial tests and stubs

---
+++ test/corgit.tests/Git/CommitTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using static corgit.Git;
+
+namespace corgit.tests
+{
+    partial class GitTests
+    {
+        [Fact]
+        public void ParseSingleParentCommit()
+        {
+            const string GIT_OUTPUT_SINGLE_PARENT = "52c293a05038d865604c2284aa8698bd087915a1\n"
+                                                    + "john.doe@mail.com\n"
+                                                    + "8e5a374372b8393906c7e380dbb09349c5385554\n"
+                                                    + "This is a commit message.";
+
+            var expected = new GitCommit("52c293a05038d865604c2284aa8698bd087915a1",
+                                         "This is a commit message.",
+                                         new[] { "8e5a374372b8393906c7e380dbb09349c5385554" },
+                                         "john.doe@mail.com");
+            var actual = Git.ParseCommit(GIT_OUTPUT_SINGLE_PARENT);
+            Assert.StrictEqual(expected, actual);
+        }
+    }
+}
---
+++ test/corgit.tests/Git/GitTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace corgit.tests
+{
+    public partial class GitTests
+    {
+        private Git Git { get; } = new Git();
+
+        [Fact]
+        public void VersionTest()
+        {
+            var result = Git.ParseVersion("git version 2.18.0.windows.1");
+            Assert.Equal("2.18.0.windows.1", result);
+        }
+    }
+}
