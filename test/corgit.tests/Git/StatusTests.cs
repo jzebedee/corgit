11c8bc7ab0486051ba37f5de0d57878160631ff0
J. Zebedee <zebedee@code.gripe>
2019-02-14 10:47:25 -0600

    Add StatusTests

---
+++ test/corgit.tests/Git/StatusTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace corgit.tests
+{
+    public class StatusTests
+    {
+        private Git Git { get; } = new Git();
+
+        [Fact]
+        public void ParseEmptyStatus()
+        {
+            const string status = "";
+
+            var results = Git.ParseStatus(status);
+            Assert.Equal(new List<GitFileStatus>(), results);
+        }
+    }
+}
