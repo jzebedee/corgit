adff09a14e67b3e8da261045a300c0f158a78181
J. Zebedee <zebedee@code.gripe>
2019-02-14 10:58:43 -0600

    Add equatability for GitFileStatus

--- src/corgit/GitFileStatus.cs
+++ src/corgit/GitFileStatus.cs
@@ -1,12 +1,57 @@
 ﻿//https://github.com/Microsoft/vscode/tree/master/extensions/git
 
+using System;
+using System.Collections.Generic;
+
 namespace corgit
 {
-    public class GitFileStatus
+    public class GitFileStatus : IEquatable<GitFileStatus>
     {
-        public char X { get; set; }
-        public char Y { get; set; }
-        public string Path { get; set; }
-        public string Rename { get; set; }
+        public GitFileStatus(char x, char y, string path, string rename)
+        {
+            X = x;
+            Y = y;
+            Path = path;
+            Rename = rename;
+        }
+
+        public char X { get; }
+        public char Y { get; }
+        public string Path { get; }
+        public string Rename { get; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as GitFileStatus);
+        }
+
+        public bool Equals(GitFileStatus other)
+        {
+            return other != null &&
+                   X == other.X &&
+                   Y == other.Y &&
+                   Path == other.Path &&
+                   Rename == other.Rename;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -865002436;
+            hashCode = hashCode * -1521134295 + X.GetHashCode();
+            hashCode = hashCode * -1521134295 + Y.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Path);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Rename);
+            return hashCode;
+        }
+
+        public static bool operator ==(GitFileStatus left, GitFileStatus right)
+        {
+            return EqualityComparer<GitFileStatus>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(GitFileStatus left, GitFileStatus right)
+        {
+            return !(left == right);
+        }
     }
 }
