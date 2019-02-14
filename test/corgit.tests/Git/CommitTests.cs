6c0ad478776e4119b32719f80f6faf21fed2b290
J. Zebedee <zebedee@code.gripe>
2019-02-14 04:59:19 -0600

    Add test ParseNoParentCommit

--- test/corgit.tests/Git/CommitTests.cs
+++ test/corgit.tests/Git/CommitTests.cs
@@ -39,5 +39,21 @@ namespace corgit.tests
             var actual = Git.ParseCommit(GIT_OUTPUT_MULTIPLE_PARENTS);
             Assert.StrictEqual(expected, actual);
         }
+
+        [Fact]
+        public void ParseNoParentCommit()
+        {
+            const string GIT_OUTPUT_NO_PARENTS = "52c293a05038d865604c2284aa8698bd087915a1\n"
+                                                       + "john.doe@mail.com\n"
+                                                       + "\n"
+                                                       + "This is a commit message.";
+
+            var expected = new GitCommit("52c293a05038d865604c2284aa8698bd087915a1",
+                                         "This is a commit message.",
+                                         null,
+                                         "john.doe@mail.com");
+            var actual = Git.ParseCommit(GIT_OUTPUT_NO_PARENTS);
+            Assert.StrictEqual(expected, actual);
+        }
     }
 }
