46b92c335e18c4491eabd86489677dc1016e7ef2
J. Zebedee <zebedee@code.gripe>
2019-02-15 00:06:20 -0600

    Add enum GitChangeType

---
+++ src/corgit/GitChangeType.cs
@@ -0,0 +1,24 @@
+﻿//https://github.com/Microsoft/vscode/tree/master/extensions/git
+
+namespace corgit
+{
+    /// <summary>
+    /// Modifier representing the status of a path
+    /// </summary>
+    /// <remarks>https://git-scm.com/docs/git-status#_short_format</remarks>
+    public enum GitChangeType : byte
+    {
+        Unmodified = (byte)' ',
+        Untracked = (byte)'?',
+        Ignored = (byte)'!',
+        Added = (byte)'A',
+        Modified = (byte)'M',
+        Deleted = (byte)'D',
+        Renamed = (byte)'R',
+        Copied = (byte)'C',
+        /// <summary>
+        /// Updated but unmerged
+        /// </summary>
+        Updated = (byte)'U',
+    }
+}
