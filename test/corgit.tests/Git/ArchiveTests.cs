a5c81ee35ea6c633844b35627248062addfe7167
J. Zebedee <zebedee@code.gripe>
2019-03-07 17:23:25 -0600

    Add archive format list parsing and test

---
+++ test/corgit.tests/Git/ArchiveTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace corgit.tests
+{
+    public class ArchiveTests
+    {
+        [Fact]
+        public void ParseFormatList()
+        {
+            const string archiveFormatList = @"tar
+tgz
+tar.gz
+zip
+";
+
+            var expected = new[] { "tar", "tgz", "tar.gz", "zip" };
+            var actual = GitParsing.ParseArchiveFormatList(archiveFormatList);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
