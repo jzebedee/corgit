77767c86f81f6542bc7e88bbac81f2ec4d6081c7
J. Zebedee <zebedee@code.gripe>
2019-03-15 05:39:41 -0400

    Add git-checkout (#11)
    * Add git-checkout
    
    * Add create option to Checkout

--- src/corgit/GitArguments.cs
+++ src/corgit/GitArguments.cs
@@ -145,6 +145,53 @@ namespace corgit
             }
         }
 
+        public struct CheckoutOptions
+        {
+            public readonly bool Track;
+            public readonly bool Create;
+            public readonly string StartPoint;
+
+            public CheckoutOptions(bool track = false, bool create = false, string startPoint = null)
+            {
+                Track = track;
+                Create = create;
+                StartPoint = startPoint;
+            }
+        }
+        public static IEnumerable<string> Checkout(string treeish, IEnumerable<string> paths = null, GitArguments.CheckoutOptions options = default)
+        {
+            if (string.IsNullOrEmpty(treeish))
+                throw new ArgumentNullException(nameof(treeish));
+
+            yield return "checkout";
+            yield return "-q";
+
+            if(options.Create)
+            {
+                yield return "-b";
+                if(!string.IsNullOrEmpty(options.StartPoint))
+                {
+                    yield return options.StartPoint;
+                }
+            }
+
+            if (options.Track)
+            {
+                yield return "--track";
+            }
+
+            yield return treeish;
+
+            if (paths != null)
+            {
+                yield return "--";
+                foreach (var path in paths)
+                {
+                    yield return path;
+                }
+            }
+        }
+
         public static IEnumerable<string> Status()
         {
             yield return "--no-optional-locks";
